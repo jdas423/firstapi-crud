65262284044c48265c298965cef1814aa7c1c091
jdas423 <jagdishdas.aps@gmail.com>
2024-05-17 18:39:13 +0530

    commited

--- main.py
+++ main.py
@@ -9,4 +9,7 @@ app.include_router(user)
 app.include_router(project)
 
 
-uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
