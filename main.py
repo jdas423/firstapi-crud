0d125a96454b6f9629fa8bab35bdabde12ab1f50
jdas423 <jagdishdas.aps@gmail.com>
2024-05-17 16:24:23 +0530

    commited

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from routes.project import project
+from routes.user import user
 import uvicorn
 
 app = FastAPI()
+app.include_router(user)
 app.include_router(project)
 
 
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000)
 
